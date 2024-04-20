fe22967299898f1177782e161a707f39bd11c1a7
DaveGev <167614516+DaveGev@users.noreply.github.com>
2024-04-20 13:54:57 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,165 @@
+// let x = 4;
+// let sum=1+2;
+// x++; //x=x+1
+// x--; //x=x-1
+// x+=2; // x=x+2
+// x-=2; // x=x-2
+// x*=2; // x=x*2
+// x/=2; // x=x/2
+// x%=2; // x=x%2
+// console.log(x);
+
+// let time = 4;
+// if (time>10 || time<16)  {
+//     console.log('Good morning');
+// }
+
+// let a=250;
+// if (a>100 && a<500) {
+//     console.log('ok');
+// }
+
+// let  lvl = 58;
+// if (lvl>50) {
+//     console.log('you got to the 50 lvl');
+// }
+// else if (lvl<50){
+//     console.log('you dont got to the 50lvl');
+// }
+
+// let weather ='sun';
+// let c =14;
+// if(weather='sun' || c>16){
+//     console.log('dzer jamn e durs galu');
+// }else{
+//     console.log('stay home');
+
+// let weather='sun';
+// let c=14;
+// if(weather='rain' && c>16){
+//     console.log('dzer jamn e durs galu');
+// }else if(weather ='rain' && c>16){
+//     console.log('durs galuc taq hagnveq');
+// }else{
+//     console.log('stay home');
+// }
+
+// let tariq=11;
+
+// if (tariq>18 || tariq == 18){
+//     console.log('duq grancveciq');
+// }else if (tariq<18){
+//     console.log('duq poqreq ays kayqi hamar');
+// }
+
+// let a=5;
+// let b=7;
+// let res = b>a;
+
+ // console.log(res);
+
+// let test =String(12345);
+
+// alert(Number(test[0])+Number(test[1])+Number(test[2])+Number(test[3])+Number(test[4]));
+
+// let test =String(12345);
+
+// alert(Number(test[0])*Number(test[1])*Number(test[2])*Number(test[3])*Number(test[4]));
+
+// let name='tom';
+// console.log(typeof name);
+// let age=15;
+// console.log(typeof age);
+// let isenabled = true;
+// console.log(typeof isenabled);
+// let un;
+// console.log(typeof un);
+// console.log(typeof (undefined));
+// console.log(typeof(null));
+// console.log(typeof(NaN));
+
+// let a=5;
+// let b=a++;
+// console.log(a);
+// console.log(b);
+
+// let y=5;
+// let z=++y;
+// console.log(y);
+// console.log(z);
+
+// let a=1297334172983;
+
+// if (a%2==0) {
+//     console.log('zuyga');
+// }else {
+//     console.log('kenta');
+// }
+
+// let b=7;
+
+// if(b%2){
+//     console.log('kent');
+// }else{
+//     console.log('zuyg');
+// }
+
+// let test1=3;
+// let test2=20;
+// let test3=test1-test2;
+
+// if (test3>0 || test3==0){
+//     console.log('drakan');
+// }else{
+//     console.log('bacasakan');
+// }
+
+// for(let i=0; i <=100; i++){
+//     console.log(i);
+// }
+
+// for(let i=1; i <=100; i+=2){
+//     console.log(i);
+// }
+
+// for(let i=0; i <=100; i+=2){
+//     console.log(i);
+// }
+
+// for (let i=10; i>=0; i--){
+//     console.log(i);
+// }
+
+// for (let i=10; i>0; i-=2){
+//     console.log(i);
+// }
+
+// for (let i=11; i>0; i-=2){
+//     console.log(i);
+// }
+
+// for (let i=0; i<10; i++){
+//     if(i%2){
+//     console.log(i);
+//     }
+// }
+
+// for(let i=0; i <=100; i++){
+//     console.log(i);
+// }
+
+// for(let i=11; i <=33; i++){
+//     console.log(i);
+// }
+
+// for(let i=0; i <=100; i+=2){
+//     console.log(i);
+// }
+
+// for(let i=1; i <=99; i+=2){
+//     console.log(i);
+// }
+
+// for(let i=100; i>=0; i--){
+//     console.log(i);
+// }
